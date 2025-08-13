15fca68c7ca85ef5b15ed1ba186e09323c53f1a7
Alejandro-9 <Alessandro.santos.nascimento@escola.pr.gov.br>
2025-08-13 20:52:14 -0300

    aula 8

---
+++ script.js
@@ -0,0 +1,9 @@
+Window.sr = ScrollReveal ( reset : true );
+
+sr.reveal ( "#image" , { duration : 1000 } );
+
+sr.reveal ( "#textoconteudo" , { 
+rotate: { x=0,y=100,z=0 },
+duration: 1500 } 0;
+
+sr.reveal ( "#formulario", { durate:1000} );
